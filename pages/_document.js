e77f1de8ddeac93d890cdc5fd6984ca826663ae7
Hernan Garat <garat.hernan@gmail.com>
2022-10-03 09:23:35 -0300

    scrollbar redesign and meta

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { CssBaseline } from '@nextui-org/react';
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { CssBaseline } from "@nextui-org/react";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
     return {
       ...initialProps,
-      styles: React.Children.toArray([initialProps.styles])
+      styles: React.Children.toArray([initialProps.styles]),
     };
   }
 
   render() {
     return (
       <Html lang="en">
-        <Head title='JSON2FORM'>{CssBaseline.flush()}</Head>
+        <Head title="JSON2FORM">
+          <meta property="og:url" content="https://www.json2form.com" />{" "}
+          <meta
+            property="og:description"
+            content="JSON2FORM is a web application that allow you to paste your json and edit in a form without complications"
+          />
+          <meta property="og:image" content={"https://imgur.com/uB8QEVq"} />
+          <meta property="og:type" content="website" /> {CssBaseline.flush()}
+        </Head>
         <body>
           <Main />
           <NextScript />
